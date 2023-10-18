f53fb00598b10ec7781635e7f908f319c19d90ba
sejinpark2 <>
2023-10-18 15:53:47 +0900

    Update DataController.java

--- DBControl1/src/main/java/DataController.java
+++ DBControl1/src/main/java/DataController.java
@@ -6,14 +6,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class DataController {
-    String urlName  = "https://script.google.com/macros/s/AKfycbyWIqG1Mt1QTgCw1ihed7UAa2efXqqmr2YgFFtRaalAMKndsrQzp4IiuMfbpDYmRSV-Nw/exec";
+    private String urlName  = null;
+    private URL url = null;
+    HttpURLConnection connection = null;
 
-    public void crud(String act,String line,String name,String age,String email){
+    public DataController() {
+        urlName = "https://script.google.com/macros/s/AKfycbyWIqG1Mt1QTgCw1ihed7UAa2efXqqmr2YgFFtRaalAMKndsrQzp4IiuMfbpDYmRSV-Nw/exec";
+    }
+
+    public void crud(String act, String line, String name, String age, String email){
         try {
+            url = new URL(urlName);
+            connection = (HttpURLConnection) url.openConnection();
             // 배포 url
-            URL url  = new URL(urlName);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type","application/json; utf-8");
             connection.setDoOutput(true);
@@ -24,16 +29,23 @@ public class DataController {
                     "\"age\": \"" + age +"\", " +
                     "\"email\": \""+ email +"\"}";
 
-            try(OutputStream os = connection.getOutputStream()){
-                byte[] input = jsonData.getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }
+            sendData(jsonData);
 
             if (act.equals("read")){
                 printData(connection);
             }
 
             System.out.println(connection.getResponseCode());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendData(String jsonData){
+        try(OutputStream os = connection.getOutputStream()){
+            byte[] input = jsonData.getBytes("utf-8");
+            os.write(input, 0, input.length);
         }
         catch (Exception e){
             e.printStackTrace();
